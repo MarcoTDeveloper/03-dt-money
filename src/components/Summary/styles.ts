bf7949a393bfc8ecb62de44f27db2d8beec6c196
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-12-11 11:32:00 -0300

    v23.1.0

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 
 export const SummaryContainer = styled.section`
     width: 100%;
@@ -11,4 +11,31 @@ export const SummaryContainer = styled.section`
     gap: 2rem;
 
     margin-top: -5rem;
+`
+
+interface SummaryCardProps {
+    variant?: 'green'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+    background-color: ${({theme}) => theme["gray-600"]};
+    border-radius: 6px;
+    padding: 2rem;
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        color: ${({theme}) => theme["gray-300"]};
+    }
+
+    strong {
+        display: block;
+        margin-top: 1rem;
+        font-size: 2rem;
+    }
+
+    ${({variant}) => variant === 'green' && css`
+        background: ${({theme}) => theme["green-700"]};
+    ` }
 `
