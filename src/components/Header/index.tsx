5eae4e2f4d8e0967c38e4d687369a6ccc93e4597
Marco Túlio Rodrigues da Silveira <marcosdevprofessional@gmail.com>
2023-12-11 16:58:51 -0300

    v23.1.0

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
-import logoDTMoney from "../../assets/logo.svg"
+import * as Dialog from '@radix-ui/react-dialog';
+
+import logoDTMoney from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -8,7 +11,15 @@ export function Header() {
             <HeaderContent>
                 <img src={logoDTMoney} />
 
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     )
